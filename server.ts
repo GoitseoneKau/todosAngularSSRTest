9fd8056e5ff93168c308504e14bda44d6bf02fb1
GoitseoneKau <66080430+GoitseoneKau@users.noreply.github.com>
2024-12-12 15:24:24 +0200

    update vercel file

--- server.ts
+++ server.ts
@@ -48,7 +48,7 @@ export function app(): express.Express {
 }
 
 export function run(): void {
-  const port =  4000;
+  const port =  process.env['PORT'] || 4000;
 
   // Start up the Node server
   const server = app();
@@ -57,4 +57,3 @@ export function run(): void {
   });
 }
 
-run();
