fcff49fc8cbbf6e4e4908fbe75c8c79434327ca0
GoitseoneKau <66080430+GoitseoneKau@users.noreply.github.com>
2024-12-17 11:07:36 +0200

    vercel update 17.12.2024

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
-const server = require('../dist/todo-app/server/server.mjs');
+// const server = require('../dist/todo-app/server/server.mjs');
 
-module.exports = server.app()
+// module.exports = server.app()
+
+export default import('../dist/todo-app/server/server.mjs').then(module=>module.app())
