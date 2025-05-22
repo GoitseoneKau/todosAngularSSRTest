a91a0bbbca51c29541a3c5515c22d82906590f0d
GoitseoneKau <66080430+GoitseoneKau@users.noreply.github.com>
2025-05-22 12:07:06 +0200

    change api/index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
-const server = require('../dist/todo-app/server/server.mjs');
-console.log("starting server function")
-module.exports = server.app()
+// const server = require('../dist/todo-app/server/server.mjs');
+// console.log("starting server function")
+// module.exports = server.app()
+export default import('../dist/todo-app/server/server.mjs').then(module=>module.app())
